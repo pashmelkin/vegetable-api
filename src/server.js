7920e7d46d2f6e8b74cc574d942db040257b397e
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-01-03 16:42:18 +1100

    A bit of restructring

--- src/server.js
+++ src/server.js
@@ -4,22 +4,24 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('vegetable-api:server');
-var http = require('http');
+const app = require('./app');
+const debug = require('debug')('vegetable-api:server');
+const http = require('http');
+const config = require('../config');
+
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(config.express.port || '3000');
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -34,7 +36,7 @@ server.on('listening', onListening);
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
