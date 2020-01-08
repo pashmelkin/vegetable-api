154df1ed666c2ce9a18052f913d306484371c012
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-01-08 11:09:42 +1100

    Add simple test for route

--- test/middleware/readVeggies_test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert')
-const veggies = require('../../src/middleware/readVeggies')
-
-describe('readVeggies', function () {
-    it('should return  when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1)
-    })
-
-})
--- test/routes/veggies_test.js
+++ test/routes/veggies_test.js
@@ -1,8 +1,12 @@
-var assert = require('assert')
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1)
-    })
+const supertest = require('supertest')
+
+const app = require('../../src/server')
+const request = supertest(app)
+
+describe('GET /v1/veggies', function () {
+  it('responds with json', function (done) {
+    request
+      .get('/v1/veggies')
+      .expect(200, done)
   })
 })
