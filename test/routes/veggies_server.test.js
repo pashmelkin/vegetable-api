07d86f22905b396cfe37aef2a30375c5beb53779
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-01-15 11:32:19 +1100

    Add all tests

--- test/routes/veggies_server.test.js
+++ test/routes/veggies_server.test.js
@@ -2,12 +2,12 @@ const supertest = require('supertest')
 const expect = require('chai').expect
 const sinon = require('sinon')
 
-const app = require('../../src/server')
+const server = require('../../src/server')
 const veggiesReader = require('../../src/middleware/readVeggies')
 
-const request = supertest(app)
+const request = supertest(server)
 
-describe('GET /v1/veggies', function () {
+describe('GET /v1/veggies is routed correctly from the server', function () {
   var readVeggiesStub
   before(function () {
     readVeggiesStub = sinon.stub(veggiesReader, 'readVeggies')
@@ -15,30 +15,14 @@ describe('GET /v1/veggies', function () {
   })
 
   after(function () {
-    veggiesReader.readVeggies.restore()
+    readVeggiesStub.restore()
   })
 
   it('responds with json: async version', async function () {
     const response = await request.get('/v1/veggies')
 
     expect(response.status).to.equal(200)
-    //  expect(response.body).to.equal({ veggies: 'tomato' })
     expect(readVeggiesStub.calledOnce).to.be.true
   })
   it('responds with json: callback version')
-  it.skip('responds with json: check the send functions is called', async function () {
-    const response = await request.get('/v1/veggies')
-
-    const res = {
-      send: function () {}
-    }
-
-    // mock res
-    const mock = sinon.mock(res)
-    // build how we expect it t work
-    mock.expects('send').once()
-
-    // verify that mock works as expected
-    mock.verify()
-  })
 })
